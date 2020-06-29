bea911983586cccb64b4abbfc832566c78059d3a
SOMNATH <schatterjee95.sc@gmail.com>
2020-06-30 01:35:33 +0530

    Added new Submit Button

---
+++ app/attach.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+<body>
+
+Name: <?php echo $_GET["fname"]; ?><br>
+Phone No: <?php echo $_GET["fphoneNo"]; ?><br>
+COURSE CHOICE: <?php echo $_GET["b"]; ?><br>
+COURSE CHOICE: <?php echo $_GET["m"]; ?><br>
+Description: <?php echo $_GET["fdescription"]; ?><br>
+SUBJECT INTERESTS: <?php echo $_GET["ai"]; ?><br>
+SUBJECT INTERESTS: <?php echo $_GET["wd"]; ?><br>
+SUBJECT INTERESTS: <?php echo $_GET["ad"]; ?><br>
+
+
+</body>
+</html>
